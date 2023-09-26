f5ef3fadf1bfabee156941d5f04592d4715086fb
Abraão Duarte <abraaoduarte@ip-192-168-0-103.ec2.internal>
2023-09-26 17:35:47 -0300

    feat: add routes

--- router/router.go
+++ router/router.go
@@ -3,12 +3,10 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize router
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(router)
+	// Run server
 	router.Run(":8080")
 }
