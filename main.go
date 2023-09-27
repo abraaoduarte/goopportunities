00508b021000a7b8dd288c95b6964415f7e3bc3e
Abraão Duarte <abraao.n.duarte@gmail.com>
2023-09-27 14:28:01 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/abraaoduarte/goopportunities/router"
+import (
+	"github.com/abraaoduarte/goopportunities/config"
+	"github.com/abraaoduarte/goopportunities/router"
+)
 
+var (
+	logger *config.Logger
+)
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	// Initialize router
 	router.Initialize()
 
